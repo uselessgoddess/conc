869811bd324dc6c44b6f5a144c91fe794dc1a01d
uselessgoddess <dodickgod@gmail.com>
2025-11-19 19:41:05 +0300

    use real c++23 to avoid uaf: - addition reserved `reset` state - `thread_local` getter + singleton instead of raw `static` - fix missing `hlt` in loop - TODO: run `containers` tests

--- hazard/domain.hpp
+++ hazard/domain.hpp
@@ -1,95 +1,126 @@
 #pragma once
 
 #include <atomic>
-#include <new>
 #include <type_traits>
 #include <vector>
 #include <array>
 #include <cassert>
+#include <memory>
+#include <cstdint>
+#include <algorithm>
 
 #include <allocator.hpp>
 
 namespace conc {
 
 template<typename T>
-struct alignas(std::hardware_destructive_interference_size) 
-domain_cell {
-    std::atomic<T*> pointer;
+struct alignas(CACHELINE_SIZE) domain_cell {
+    std::atomic<T*> pointer = nullptr;
 };
 
 struct default_placeholder {};
 
-template<typename T, std::size_t max_objects = 128, typename placeholder = default_placeholder>
+template<typename T, std::size_t max_objects = 128, typename _ = default_placeholder>
 requires(std::is_nothrow_destructible_v<T>)
-class hazard_domain {
-   public:
+struct hazard_domain {
+    struct reserved { // im very lazy.nvim to declare friend class
+        static T* sentinel() noexcept {
+            return reinterpret_cast<T*>(~static_cast<std::uintptr_t>(0));
+        }
+        static T* reset() noexcept { // pretty new state
+            return reinterpret_cast<T*>(~static_cast<std::uintptr_t>(0) - 1);
+        }
+    };
+
+    hazard_domain() noexcept {
+        if (!m_instance)
+            m_instance = this;
+    }
+
+    [[nodiscard]]
     domain_cell<T>* capture_cell() noexcept {
-        T* null;
-
-        auto it = m_acquire_list.begin();
-        for(;it != m_acquire_list.end(); ++it) {
-            null = nullptr;
-
-            if(it->pointer.compare_exchange_strong(
-                null,
-                SENTINEL
-            )) {
-                break;
+        for (auto& cell : m_acquire_list) {
+            T* expected = nullptr;
+            if (cell.pointer.load(std::memory_order_relaxed) == nullptr &&
+                cell.pointer.compare_exchange_strong(expected, reserved::sentinel(), std::memory_order_acq_rel)) {
+                return &cell;
             }
         }
+        // todo! avoid `abort` or break everything with unreachable
+        std::abort();
+    }
 
-        assert(it != m_acquire_list.end());
-
-
-        return &(*it);
+    void release_cell(domain_cell<T>* cell) noexcept {
+        if (cell) {
+            cell->pointer.store(nullptr, std::memory_order_release);
+        }
     }
 
     void retire(T* data) {
-        if(tl_retire.size() > max_objects * 2) {
-            delete_hazards();
-        }
+        auto& retires = tl_retires();
 
-        /*thread_local*/ tl_retire.emplace_back(data);
-    }
-
-    void delete_hazards() noexcept {
-        for(std::size_t i = 0; i < tl_retire.size(); ++i) {
-            if(!scan_for_hazard(tl_retire[i])) {
-                delete tl_retire[i];
-                tl_retire[i] = tl_retire[tl_retire.size() - 1];
-                tl_retire.pop_back();
-                i--;
-            }
+        retires.push_back(data);
+        if (retires.size() >= 2 * max_objects) {
+            delete_hazards(retires);
         }
     }
 
    private:
-    bool scan_for_hazard(T* pointer) noexcept {
-        for(auto it = m_acquire_list.begin(); it != m_acquire_list.end(); ++it) {
-            [[unlikely]]
-            if(pointer == it->pointer.load()) {
+    void delete_hazards(std::vector<T*>& buffer) noexcept {
+        auto it = buffer.begin();
+        auto end = buffer.end();
+
+        while (it != end) {
+            if (scan_for_hazard(*it)) {
+                ++it;
+            } else {
+                delete *it;
+                --end;
+                if (it != end) {
+                    *it = std::move(*end);
+                }
+            }
+        }
+
+        buffer.erase(end, buffer.end());
+    }
+
+    [[nodiscard]]
+    bool scan_for_hazard(T* ptr) noexcept {
+        auto* sentinel = reserved::sentinel();
+        auto* reset = reserved::reset();
+
+        for (const auto& cell : m_acquire_list) {
+            T* hazard = cell.pointer.load(std::memory_order_seq_cst);
+            if (hazard != nullptr && hazard != sentinel &&
+                hazard != reset && hazard == ptr) [[unlikely]] {
                 return true;
             }
         }
-
         return false;
     }
 
-   private:
-    inline static
-     std::array<domain_cell<T>, max_objects> m_acquire_list;
+    static auto tl_retires() -> std::vector<T*>& {
+        thread_local auto retires = retire_buffer(m_instance);
+        return retires.buffer;
+    }
 
-    inline static thread_local
-     std::vector<T*> tl_retire;
+    struct retire_buffer {
+        std::vector<T*> buffer;
+        hazard_domain* domain_ref = nullptr;
 
-    // placeholder value to a aligned storage to mark cell that is captured and yet to be used
-    // could use reinterpreted cast to domain address, but made for compiler/standard grooming
-    alignas(T) inline static 
-     char sentinel_storage[sizeof(T)];
+        explicit retire_buffer(hazard_domain* d) : domain_ref(d) {
+            buffer.reserve(max_objects * 3);
+        }
 
-    inline static 
-     T* const SENTINEL = reinterpret_cast<T*>(&sentinel_storage);
+        ~retire_buffer() {
+            if (domain_ref && !buffer.empty()) {
+                domain_ref->delete_hazards(buffer);
+            }
+        }
+    };
+
+    inline static std::array<domain_cell<T>, max_objects> m_acquire_list;
+    inline static hazard_domain* m_instance = nullptr; // more portable but still broekn on mingw
 };
-
 }
-
